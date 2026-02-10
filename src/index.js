e93ffba6e21bd50b243bf46cff3b38128f68ff35
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2026-02-10 14:29:05 +0700

    create layout using handlebar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const path = require('path');
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const port = 3000;
 // https:localhost:3000
 
 //https
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
+//template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.get("/", (req, res) => {
   // res.send('Hello World! 123')
-   res.send(`
-    <h1>Hello World!</h1>
-    `)
-})
+  res.render("home");
+});
+
+app.get("/new", (req, res) => {
+  // res.send('Hello World! 123')
+  res.render("new");
+});
 //127.0.0.1
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
