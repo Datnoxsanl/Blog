6139de7ecf6f1289149bacce89f4adedd6c17824
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2026-02-12 16:32:57 +0700

    MVC Refeactor code: create folder routes,app/controllers

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
-
 const app = express();
 const port = 3000;
 // https:localhost:3000
 
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.join(__dirname, "public")));
 // http://localhost:3000/img/logo.jpg
 
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 
 //https
 app.use(morgan("combined"));
@@ -25,30 +22,13 @@ app.engine(
   "hbs",
   engine({
     extname: ".hbs",
-  })
+  }),
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  // res.send('Hello World! 123')
-  res.render("home");
-});
-
-app.get("/new", (req, res) => {
-  res.render("new");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
-
-
+// Routes init
+route(app);
 
 //127.0.0.1
 app.listen(port, () => {
