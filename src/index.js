3aee4b7f343e9700b4b44cc84888c31e00241bd3
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2026-02-19 15:38:19 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 // https:localhost:3000
 
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // http://localhost:3000/img/logo.jpg
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //https
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //template engine
-        app.engine(
-    'hbs',
-       
-    
-     
-    
-     engine({
-        extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-app.set('views', 
-                    path.join(__dirname,                 'resources', 'views'));
+app.engine( "hbs",engine({  extname: ".hbs",}),);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 // Routes init
 route(app);
 
 //127.0.0.1
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
