e0b77ea05fd3f6909c78f45841ac3f99fbf715b8
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2026-02-19 15:35:40 +0700

    install prettier, lint staged, husky=> Code Formatter

--- README.md
+++ README.md
@@ -13,3 +13,14 @@
 * npm install handlebar
 
 * static file and scss
+
+* prettier , lint-staged, husky => github 
+npm i prettier lint-staged husky --save-dev
+
+Prettirer: [Docs](https://prettier.io/docs/cli.html)
+
+npm run beauty
+
+lint-staged: example:git status =>  git add  => git status => npm run beauty
+
+husky: 
--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,12 @@
 class NewsController {
-  // [GET] /new
-  index(req, res) {
-    res.render("news");
-  }
-  // [GET] /news/:slug
-  show(req,res){
-    res.send("NEWS DETAIL");
-  }
+    // [GET] /new
+    index(req, res) {
+        res.render('news');
+    }
+    // [GET] /news/:slug
+    show(req, res) {
+        res.send('NEWS DETAIL');
+    }
 }
 
 module.exports = new NewsController();
